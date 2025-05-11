28490f2ea9bff5ddc7e3f7973be7d49196a699ff
Yvette334 <y.muhoracye@alustudent.com>
2025-05-11 10:30:52 +0200

    Delete modules directory

--- modules/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""init magic method for models directory"""
-from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
-
-
-storage = FileStorage()
-storage.reload()
