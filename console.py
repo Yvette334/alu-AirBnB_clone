b7d27f1c39120df97b749b0ff7864eaf985dd8a7
Yvette334 <y.muhoracye@alustudent.com>
2025-05-11 17:21:38 +0200

    add

--- console.py
+++ console.py
@@ -34,7 +34,7 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_EOF(self, arg):
-        """EOF command to exit the program"""
+        """Exit the programme in non-interactive mode"""
         print()
         return True
 
@@ -96,8 +96,9 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
 
     def do_all(self, arg):
-        """Prints all string representation of all instances
-        Usage: all or all <class name>
+        """Usage: all or all <class> or <class>.all()
+        Display string representations of all instances of a given class.
+        If no class is specified, displays all instantiated objects.
         """
         args = shlex.split(arg)
         objects = storage.all()
@@ -139,67 +140,22 @@ class HBNBCommand(cmd.Cmd):
             obj = objects[key]
             attr_name = args[2]
             attr_value = args[3]
-
+            
             # Skip protected attributes
             if attr_name in ["id", "created_at", "updated_at"]:
                 return
-
+            
             # Try to convert value to appropriate type
             try:
                 if attr_value.isdigit():
                     attr_value = int(attr_value)
-                elif (attr_value.replace('.', '', 1).isdigit() and
-                      attr_value.count('.') < 2):
+                elif attr_value.replace('.', '', 1).isdigit() and attr_value.count('.') < 2:
                     attr_value = float(attr_value)
             except AttributeError:
                 pass
-
+            
             setattr(obj, attr_name, attr_value)
             obj.save()
-
-    def default(self, arg):
-        """Default behavior for cmd module when input is invalid"""
-        arg_list = arg.split('.')
-        if len(arg_list) == 1:
-            print("*** Unknown syntax: {}".format(arg))
-            return
-
-        class_name = arg_list[0]
-        command = arg_list[1].split('(')[0]
-        args = arg_list[1].split('(')[1].rstrip(')')
-
-        # Handle different command formats
-        if command == 'all':
-            self.do_all(class_name)
-        elif command == 'count':
-            self.do_count(class_name)
-        elif command == 'show':
-            id = args.strip('"\'')
-            self.do_show(class_name + ' ' + id)
-        elif command == 'destroy':
-            id = args.strip('"\'')
-            self.do_destroy(class_name + ' ' + id)
-        elif command == 'update':
-            # Check for dictionary update format
-            dict_match = re.search(r"([^{]*),\s*({.*})(.*)", args)
-            if dict_match:
-                id = dict_match.group(1).strip('\'"')
-                attr_dict = eval(dict_match.group(2))
-                for attr_name, attr_value in attr_dict.items():
-                    self.do_update('{} {} {} "{}"'.format(
-                        class_name, id, attr_name, attr_value))
-            else:
-                # Handle regular update format
-                args_list = args.split(',')
-                id = args_list[0].strip('\'"')
-                if len(args_list) > 1:
-                    attr_name = args_list[1].strip('\'" ')
-                    if len(args_list) > 2:
-                        attr_value = args_list[2].strip('\'" ')
-                        self.do_update('{} {} {} "{}"'.format(
-                            class_name, id, attr_name, attr_value))
-        else:
-            print("*** Unknown syntax: {}".format(arg))
 
     def do_count(self, arg):
         """Counts the number of instances of a class
@@ -216,6 +172,58 @@ class HBNBCommand(cmd.Cmd):
                 if args[0] == obj.__class__.__name__:
                     count += 1
             print(count)
+
+    def do_models(self, arg):
+        """List all available models"""
+        print(", ".join(self.valid_classes))
+
+    def default(self, arg):
+        """Default behavior for cmd module when input is invalid"""
+        arg_list = arg.split('.')
+        if len(arg_list) == 1:
+            print("*** Unknown syntax: {}".format(arg))
+            return
+        
+        class_name = arg_list[0]
+        command = arg_list[1].split('(')[0]
+        args = arg_list[1].split('(')[1].rstrip(')')
+        
+        # Handle different command formats
+        if command == 'all':
+            self.do_all(class_name)
+        elif command == 'count':
+            self.do_count(class_name)
+        elif command == 'show':
+            id = args.strip('"\'')
+            self.do_show(class_name + ' ' + id)
+        elif command == 'destroy':
+            id = args.strip('"\'')
+            self.do_destroy(class_name + ' ' + id)
+        elif command == 'update':
+            # Check for dictionary update format
+            dict_match = re.search(r"([^{]*),\s*({.*})(.*)", args)
+            if dict_match:
+                id = dict_match.group(1).strip('\'"')
+                try:
+                    attr_dict = eval(dict_match.group(2))
+                    for attr_name, attr_value in attr_dict.items():
+                        update_cmd = '{} {} {} "{}"'.format(
+                            class_name, id, attr_name, attr_value)
+                        self.do_update(update_cmd)
+                except Exception as e:
+                    print("*** Error: {}".format(e))
+            else:
+                # Handle regular update format
+                args_list = args.split(',')
+                id = args_list[0].strip('\'"')
+                if len(args_list) > 1:
+                    attr_name = args_list[1].strip('\'" ')
+                    if len(args_list) > 2:
+                        attr_value = args_list[2].strip('\'" ')
+                        self.do_update('{} {} {} "{}"'.format(
+                            class_name, id, attr_name, attr_value))
+        else:
+            print("*** Unknown syntax: {}".format(arg))
 
 
 if __name__ == '__main__':
